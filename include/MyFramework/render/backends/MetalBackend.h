628d55e8f4808608948afb71fd997f6915ad9a39
Omega <emomega@proton.me>
2026-04-11 16:19:09 -0400

    Create MetalBackend.h

---
+++ include/MyFramework/render/backends/MetalBackend.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <GQFusion/render/rendererbackend.h>
+
+namespace GQFusion {
+
+/**
+ * @brief Metal rendering backend (macOS).
+ */
+class MetalBackend : public RendererBackend {
+public:
+    MetalBackend() = default;
+    ~MetalBackend() override = default;
+
+    bool initialize() override;
+    void shutdown() override;
+
+    void beginFrame() override;
+    void endFrame() override;
+    void present() override;
+
+    void drawRect(float x, float y, float w, float h) override;
+
+    void bindRenderTarget(RenderTarget* target) override;
+
+    // Compute
+    void bindComputeShader(void* shader) override;
+    void dispatchCompute(int x, int y, int z) override;
+};
+
+} // namespace GQFusion
