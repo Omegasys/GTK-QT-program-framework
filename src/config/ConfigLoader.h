62c9f6e2d4726bc0d2513d1354f0953260ec230a
Omega <emomega@proton.me>
2026-04-11 14:36:55 -0400

    Update ConfigLoader.h

--- src/config/ConfigLoader.h
+++ src/config/ConfigLoader.h
@@ -1,12 +1,15 @@
 #pragma once
-#include <string>
 
-class Config;
+#include <string>
+#include <MyFramework/config/Config.h>
 
 class ConfigLoader {
 public:
+    // Load config with fallback chain
     static Config load(const std::string& appName);
 
 private:
-    static std::string getDefaultPath();
+    static std::string getAppConfigPath(const std::string& appName);
+    static std::string getDefaultConfigPath();
+    static Config createDefaultConfig();
 };
