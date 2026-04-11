79e5e4c9713ffdc3119080ed2a8b56cd8eac73e6
Omega <emomega@proton.me>
2026-04-11 19:08:51 -0400

    Create PropertyEditor.cpp

---
+++ tools/ui_inspector/src/PropertyEditor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class PropertyEditor {
+public:
+    void update() {
+        // Sync selected widget properties
+    }
+
+    void draw() {
+        std::cout << "[Inspector] PropertyEditor drawing properties...\n";
+    }
+};
