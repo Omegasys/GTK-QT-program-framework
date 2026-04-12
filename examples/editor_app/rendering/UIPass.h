d9758cefecf49549af82979829ee4206b75d128a
Omega <emomega@proton.me>
2026-04-11 23:06:46 -0400

    Create UIPass.h

---
+++ examples/editor_app/rendering/UIPass.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*
+ * UIPass
+ * ------------------------------------------------------------
+ * FrameGraph pass for rendering UI.
+ */
+
+class UIPass {
+public:
+    UIPass();
+
+    void execute();
+};
