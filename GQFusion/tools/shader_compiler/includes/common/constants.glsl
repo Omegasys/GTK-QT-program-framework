a586a80d1182e3f34b560e89b09f8735d6577969
Omega <emomega@proton.me>
2026-04-11 20:37:21 -0400

    Create constants.glsl

---
+++ GQFusion/tools/shader_compiler/includes/common/constants.glsl
@@ -0,0 +1,11 @@
+#ifndef GQF_CONSTANTS_GLSL
+#define GQF_CONSTANTS_GLSL
+
+const float PI = 3.14159265359;
+const float TWO_PI = 6.28318530718;
+const float HALF_PI = 1.57079632679;
+
+const float EPSILON = 1e-5;
+const float INV_PI = 1.0 / PI;
+
+#endif
