91fb9a3034e54f253a9a3500f7d43f09eda2f43f
Omega <emomega@proton.me>
2026-04-11 23:07:28 -0400

    Create FrameGraphBuilder.h

---
+++ examples/editor_app/rendering/FrameGraphBuilder.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*
+ * FrameGraphBuilder
+ * ------------------------------------------------------------
+ * Builds the frame graph for each frame.
+ */
+
+class FrameGraphBuilder {
+public:
+    FrameGraphBuilder();
+
+    void build();
+};
