21034f5c21ce696b40c130ca01861f8e4c8b02de
Omega <emomega@proton.me>
2026-04-11 23:06:12 -0400

    Create ViewportRenderer.h

---
+++ examples/editor_app/rendering/ViewportRenderer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/*
+ * ViewportRenderer
+ * ------------------------------------------------------------
+ * Handles rendering of the main viewport.
+ */
+
+class ViewportRenderer {
+public:
+    ViewportRenderer();
+
+    void initialize();
+    void render();
+};
